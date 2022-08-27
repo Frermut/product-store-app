387372c9b658e8c70d10cd1663a2b243348ad97b
Danila Lebedkin <lebedkin_danila@mail.ru>
2022-08-27 19:45:33 +0300

    Add auth

--- script/script.js
+++ script/script.js
@@ -14,31 +14,36 @@ function renderNavbarLinks() {
     const list = document.querySelector("#nav-list");
     const navbarLinks = [{
             title: "Home",
-            icon: "bi bi-house"
+            icon: "bi bi-house",
+            id: 1
         },
         {
             title: "About",
-            icon: "bi bi-info-square"
+            icon: "bi bi-info-square",
+            id: 2
         },
         {
             title: "Specials",
-            icon: "bi bi-bookmark"
+            icon: "bi bi-bookmark",
+            id: 3
         },
         {
             title: "Authorization",
-            icon: "bi bi-person-fill"
+            icon: "bi bi-person-fill",
+            id: 4
         },
         {
             title: "User Cart",
-            icon: "bi bi-bag"
+            icon: "bi bi-bag",
+            id: 5
         },
     ]; //типо из БД получили инфу 
 
-    navbarLinks.forEach(function(elem) {
-        var title = elem.title;
-        var icon = elem.icon;
-        var liMarkup = `<li class="nav-item">
-                            <a class="nav-link active" 
+    navbarLinks.forEach(function(elem, id) {
+                var title = elem.title;
+                var icon = elem.icon;
+                var liMarkup = `<li class="nav-item" ${elem.id == 4 ? `data-bs-toggle="modal" data-bs-target="#login"` : ''}>
+                            <a id="navbarlink-${elem.id}" class="nav-link ${elem.id == 5 ? `disabled` : `active`}" 
                             aria-current="page" 
                             href="#" 
                             data-bs-toggle="tooltip" 
@@ -78,7 +83,7 @@ function renderSidebarLinks(data) {
 
 async function getSidebarLinks() {
     let resp = await fetch("http://www.demo-it-park.site/getApi/?categories=get");
-    let categories = await resp.json()
+    let categories = await resp.json();
     let newCategories = categories.map((element, ind) => {
         return { id: ind + 1, name: element[0].toUpperCase() + element.substring(1) };
     });
@@ -116,25 +121,35 @@ async function getDataFromApi() {
     return data;
 }
 
-async function renderProducts(data) {
-    let newProducts = data.product;
+async function renderProducts(products, catId = 1) {
+    if (catId == 1) {
+        products.forEach(product => {
+            renderProduct(product);
+        });
+    } else {
+        document.querySelector(".products").innerHTML = '';
+        products.forEach(product => {
+            if (product.categoryId == catId) {
+                renderProduct(product);
+            }
+        });
+    }
+}
 
-    // console.log(newProducts);
-
-    newProducts.forEach(product => {
-        let markup = `
+function renderProduct(product) {
+    let markup = `
         <div class="card mt-2" style="width: 18rem; " data-id="${product.id}" data-category="${product.categoryId}">
             <img class="card-img-top" style="width:220px; height:200px; align-self:center;" src="${product.img}" alt="Card image cap">
                 <div class="card-body d-flex flex-column justify-content-between">
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text">${product.description}</p>
                     <span class="text-muted d-block fs-2 mb-3">$${product.price.toFixed(2)}</span>
-                    <a href="#" class="btn btn-danger align-self-start">View</a>
+                    <a href="#" class="btn-buy btn btn-danger align-self-start">Buy</a>
                 </div>
         </div>
-    `
-        document.querySelector(".products").insertAdjacentHTML("beforeend", markup);
-    });
+    `;
+    document.querySelector(".products").insertAdjacentHTML("beforeend", markup);
+
 }
 
 function filterItems(list, data) {
@@ -144,8 +159,9 @@ function filterItems(list, data) {
             event.target.classList.add('active');
             document.querySelector(".active-category").textContent = event.target.textContent;
         }
-        let products = getProducts(data);
-        console.log(products);
+        renderProducts(data.product, event.target.dataset.id)
+            // let products = getProducts(data);
+            // console.log(products);
     });
 }
 
@@ -154,12 +170,56 @@ function tooltipInit(condition) {
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 }
 
+async function auth(login,password) {
+    const user = {login,password};
+    const resp = await fetch("http://www.demo-it-park.site/getApi/?auth=post", {
+        method: 'POST',
+        body: JSON.stringify(user),
+    });
+    const data = await resp.text();
+    localStorage.setItem('auth',data);
+    console.log(login, password, data);
+}
+
+function addItemToCard(id) {}
+
+function checkLogin() {
+    const resp = localStorage.getItem('auth');
+    const user = JSON.parse(resp);
+    console.log(user);
+    if (!user) {
+        const elemModal = document.querySelector('#login');
+        const modal = new bootstrap.Modal( elemModal);
+        modal.show();
+    } 
+    // else {
+    //     document.querySelector("#navbarlink-5").classList.toggle("disabled");
+    //     document.querySelector("#navbarlink-5").classList.toggle("active");
+    //  }
+}
+
+function eventListenersHandler() {
+    document.querySelector('#login form')?.addEventListener("submit",function(event) {
+        event.preventDefault();
+        const login = document.querySelector('#login form').elements.loginField.value;
+        const password = document.querySelector('#login form').elements.passwordField.value;
+        
+        auth(login,password);
+    });
+    document.querySelector('.products')?.addEventListener("click", function(event) {
+        if(event.target.matches('.btn-buy')){
+            checkLogin();
+        }
+        addItemToCard();
+    });
+}
 
 async function init() {
     renderNavbarLinks();
     let data = await getDataFromApi();
     renderSidebarLinks(data);
-    renderProducts(data);
+    renderProducts(data.product);
+    eventListenersHandler();
     mediaQueries();
 }
 
